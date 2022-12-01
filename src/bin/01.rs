8eadd09e16a6d89ffda14e9b53ad497bc1dc09c7
Trevor Settles <roboteng@gmail.com>
2022-12-01 11:01:58 -0700

    day 01 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 pub fn part_one(input: &str) -> Option<u32> {
     input
         .split("\n\n")
@@ -12,7 +14,18 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let food_per_elf = input.split("\n\n").map(|lines| {
+        lines
+            .lines()
+            .map(|line| line.parse::<u32>().unwrap_or(0))
+            .reduce(|a, b| a + b)
+            .unwrap()
+    });
+    let mut food: Vec<_> = food_per_elf.collect();
+    food.sort();
+    let len = food.len();
+    let last_three = food.iter().skip(len - 3).map(|&a| a);
+    last_three.reduce(|a, b| a + b)
 }
 
 fn main() {
