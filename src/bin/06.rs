cb97744775800f57bbc0bb4d36c24725e77b3f73
Trevor Settles <roboteng@gmail.com>
2022-12-06 09:29:15 -0700

    day 06 part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -3,11 +3,12 @@ use std::collections::HashSet;
 struct Streamer<'a> {
     s: &'a str,
     i: usize,
+    size: usize,
 }
 
 impl<'a> Streamer<'a> {
-    fn new(s: &'a str) -> Self {
-        Self { s, i: 0 }
+    fn new(s: &'a str, size: usize) -> Self {
+        Self { s, i: 0, size }
     }
 }
 
@@ -15,8 +16,8 @@ impl<'a> Iterator for Streamer<'a> {
     type Item = &'a str;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.i < self.s.len() - 4 {
-            let s = &self.s[(self.i)..(self.i + 4)];
+        if self.i < self.s.len() - self.size {
+            let s = &self.s[(self.i)..(self.i + self.size)];
             self.i += 1;
             Some(s)
         } else {
@@ -31,9 +32,10 @@ fn all_unique(s: &str) -> bool {
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let s = Streamer::new(input);
+    let size = 4;
+    let s = Streamer::new(input, size);
     for (i, k) in s.enumerate() {
-        let i = i + 4;
+        let i = i + size;
         if all_unique(k) {
             return Some(i as u32);
         }
@@ -42,6 +44,14 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
+    let size = 14;
+    let s = Streamer::new(input, size);
+    for (i, k) in s.enumerate() {
+        let i = i + size;
+        if all_unique(k) {
+            return Some(i as u32);
+        }
+    }
     None
 }
 
