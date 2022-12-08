a374130cbfeaf0860b647a4d275004bc353050ce
Trevor Settles <roboteng@gmail.com>
2022-12-08 14:08:34 -0700

    day 08 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -5,7 +5,7 @@ struct Forest {
 impl Forest {
     fn trees_left(&self, x: usize, y: usize) -> Vec<u8> {
         let k = &self.trees[y][0..x].to_vec();
-        k.clone()
+        k.iter().rev().map(|u| *u).collect()
     }
     fn trees_right(&self, x: usize, y: usize) -> Vec<u8> {
         let k = &self.trees[y][(x + 1)..].to_vec();
@@ -16,7 +16,7 @@ impl Forest {
         for pos in 0..y {
             vec.push(self.trees[pos][x])
         }
-        vec
+        vec.iter().rev().map(|u| *u).collect()
     }
     fn trees_down(&self, x: usize, y: usize) -> Vec<u8> {
         let mut vec = Vec::new();
@@ -37,6 +37,30 @@ impl Forest {
             .iter()
             .any(|los| los.iter().all(|t| *t < self.trees[y][x]))
     }
+
+    fn sceinic_score(&self, x: usize, y: usize) -> u32 {
+        let dirs = vec![
+            self.trees_up(x, y),
+            self.trees_left(x, y),
+            self.trees_right(x, y),
+            self.trees_down(x, y),
+        ];
+        dirs.iter()
+            .map(|trees| visible_trees(self.trees[y][x], trees))
+            .reduce(|a, b| a * b)
+            .unwrap()
+    }
+}
+
+fn visible_trees(height: u8, trees: &Vec<u8>) -> u32 {
+    let mut count = 0;
+    for tree in trees.iter() {
+        count += 1;
+        if *tree >= height {
+            return count;
+        }
+    }
+    count
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -60,7 +84,21 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let trees: Vec<Vec<u8>> = input
+        .lines()
+        .map(|l| l.chars().map(|c| c as u8 - '0' as u8).collect())
+        .collect();
+    let width = trees[0].len();
+    let height = trees.len();
+    let forest = Forest { trees };
+
+    let mut current_max = u32::MIN;
+    for x in 0..width {
+        for y in 0..height {
+            current_max = current_max.max(forest.sceinic_score(x, y));
+        }
+    }
+    Some(current_max)
 }
 
 fn main() {
@@ -82,6 +120,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 8);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(8));
     }
 }
